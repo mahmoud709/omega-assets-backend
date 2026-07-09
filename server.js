8e6fbdd9bbcc4a60199e8665a5f46914bbe7dbcf
mahmoud <mahmoudshalaby363@gmail.com>
2026-07-09 10:07:25 +0300

    fix ts-node module resolution conflict in server.js

--- server.js
+++ server.js
@@ -1,10 +1,2 @@
-// Register ts-node programmatically using local dependencies
-require('ts-node').register({
-  transpileOnly: true,
-  compilerOptions: {
-    module: "commonjs"
-  }
-});
-
-// Load the actual server entry point
+require('ts-node/register');
 require('./src/server.ts');
